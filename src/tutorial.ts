8008f59e12bc814298f99e374bd9a51d4a35ba35
Anastasiia <anasteshi13@gmail.com>
2026-07-23 13:40:15 +0300

    First exercise

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -10,3 +10,20 @@ amount = 12 - 1
 let isAwesome = false
 isAwesome = true
 // isAwesome = "shakeAndBake" // error
+
+// String
+let firstName = "Anastasiia"
+let lastName = "Brown"
+
+console.log(firstName.concat(lastName))
+console.log(firstName.slice(3, 7))
+
+// Number
+let num1 = 3
+let num2 = 5
+console.log(Math.max(num1, num2))
+
+// Boolean
+let isAdult = true
+// console.log(isAdult == "true") // cannot compare values of different types
+console.log(isAdult == true) 
