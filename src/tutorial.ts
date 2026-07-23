8c787ca621f45f0d9d2d7269141f9cb9aa81a78e
Anastasiia <anasteshi13@gmail.com>
2026-07-23 13:48:31 +0300

    Union types

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,29 +1,8 @@
-let awesomeName = "shakeAndBake"
-awesomeName = "something" // valid
-awesomeName = awesomeName.toUpperCase() // only methods of strings
-// awesomeName = 23 // error
+// with union types it is possible to set up multiple valid values or types for a variable
+let tax: number | string = 10
+tax = "$10"
+tax = 10
 
-let amount = 20
-amount = 12 - 1
-// amount = "word" // error
+let requestStatus: "pending" | "success" | "error" = "pending"
+let responseStatus: "success" | 1 | true = "success"
 
-let isAwesome = false
-isAwesome = true
-// isAwesome = "shakeAndBake" // error
-
-// String
-let firstName = "Anastasiia"
-let lastName = "Brown"
-
-console.log(firstName.concat(lastName))
-console.log(firstName.slice(3, 7))
-
-// Number
-let num1 = 3
-let num2 = 5
-console.log(Math.max(num1, num2))
-
-// Boolean
-let isAdult = true
-// console.log(isAdult == "true") // cannot compare values of different types
-console.log(isAdult == true) 
