9f1fdd28a49929c7daad916eed938f14675f394c
Anastasiia <anasteshi13@gmail.com>
2026-07-24 19:11:26 +0300

    Practical examples of type annotation

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,8 +1,23 @@
-let notSure: any = 4
-notSure = "notSure"
-notSure = false
-notSure = () => {
-    console.log(notSure)
-}
+// Practical example – type inference
+const books = ["1984", "Brave New World", "Last wish"]
+let foundBook: string | undefined
 
-let random // same as let random: any
+for (let book of books) {
+    if (book === "1984") {
+        foundBook = book
+        foundBook = foundBook.toUpperCase()
+        break
+    }
+}
+console.log(foundBook?.length)
+
+// Practical example – union type
+let orderStatus: "processing" | "shipped" | "delivered"
+orderStatus = "processing"
+orderStatus = "delivered"
+// orderStatus = "cancelled" // is not valid
+
+let discount: number | string
+discount = 20
+discount = "$20"
+// discount = true // is not valid
