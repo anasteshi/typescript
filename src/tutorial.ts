f4afa3195f8e04a47a3cce223832c32056f6a793
Anastasiia <anasteshi13@gmail.com>
2026-08-24 21:51:24 +0300

    Object fundamentals

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,9 +1,18 @@
-// Practical example – arrays
-let temperatures: number[] = [30, 22, 42]
-// temperatures.push("22 degrees") // is not valid
+let car: { brand: string; year: number } = { brand: "toyota", year: 2020 }
 
-let colors: string[] = ["black", "white"]
-// colors.push(true) // is not valid
+car.brand = "ford" // valid
 
-let mixedArray: (number | string)[] = ["jack", 100, "anna"]
-// mixedArray.unshift(false) // not valid
+let car1: { brand: string; year: number } = { brand: "audi", year: 2022 }
+
+let book = { title: "book", cost: 20 }
+let pen = { title: "pen", cost: 5 }
+let notebook = { title: "notebook" }
+
+let items: { title: string; readonly cost?: number }[] = [
+    book,
+    pen,
+    notebook,
+] // ?: optional property
+
+// items[0].cost = 0 // invalid since cost is readonly
+items[0].title = "free book"
