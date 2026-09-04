6c6f8526ed242f6b2221a131fac5d82a904060d5
Anastasiia <anasteshi13@gmail.com>
2026-09-04 11:13:11 +0300

    Default function parameters

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -4,3 +4,15 @@ function calculatePrice(price: number, discount?: number): number {
 }
 
 let priceAfterDiscount = calculatePrice(100, 20)
+
+function calculateScore(
+    initialScore: number,
+    penaltyPoints: number = 0,
+): number {
+    // = 0 is default function parameter syntax
+
+    return initialScore - penaltyPoints
+}
+
+let scoreAfterPenalty = calculateScore(100 - 20)
+let scoreWithoutPenalty = calculateScore(300)
