5ec40b9257a094ab98a754bd0c15c1f8304782b1
Anastasiia <anasteshi13@gmail.com>
2026-09-07 12:44:34 +0300

    Practical example of function default parameters and union type parameters

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,21 +1,16 @@
-function createEmployee({ id }: { id: number }): {
-    id: number
-    isActive: boolean
-} {
-    return { id, isActive: id % 2 === 0 }
+function processData(
+    input: string | number,
+    config: { reverse: boolean } = { reverse: false },
+): string | number {
+    if (typeof input === "number") {
+        return input ** 2
+    } else {
+        return config.reverse ?
+                input.toUpperCase().split("").reverse().join("")
+            :   input.toUpperCase()
+    }
 }
 
-const first = createEmployee({ id: 1 })
-const second = createEmployee({ id: 2 })
-
-console.log(first, second)
-
-// alternative
-function createStudent(student: { id: number; name: string }): void {
-    console.log(
-        `Welcome to the course, ${student.name.toUpperCase()}. Your id is: ${student.id}`,
-    )
-}
-
-const newStudent = { id: 20, name: "Anastasiia" }
-console.log(createStudent(newStudent))
+console.log(processData(10))
+console.log(processData("Hello!"))
+console.log(processData("Hello!", { reverse: true }))
